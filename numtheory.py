042c3e4b5fb1c807a34fbc2e4868e9d85c74bbb7
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-01-06 17:40:54 +1100

    Add files via upload

---
+++ numtheory.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+def is_prime(n):
+    """Check if an integer n is a prime number."""
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
+ 
+def Charles():
+	plt.figure(dpi=120, figsize=[12, 7])
+	x = []
+	for n in range(0, 1024):
+		if is_prime(n) == True:
+			x.append(n)
+	y = []
+	x2 = []
+	for n in range(len(x)):
+		for e in x:
+			y.append(e ** n)
+			print("e: {}, n: {}, e**n: {}".format(e, n, e **n))
+#	print(x)
+#	print(len(y))
+	print(y)
+if __name__ == """__main__""": Charles()
