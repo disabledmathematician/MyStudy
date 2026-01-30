a8afdd5c009f2a4ea49dda24393d100a49458637
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-01-30 17:46:53 +1100

    Add files via upload

--- primes.py
+++ primes.py
@@ -1,5 +1,6 @@
 import math
 from time import sleep
+import humanize
 def is_prime(n):
     if n <= 1:
         return False
@@ -15,14 +16,29 @@ def is_prime(n):
     return True
  
 def Charles():
+	countPythagorean = 1
+	countMersenne = 1
 	for n in range(1, 1024):
 		q = 4 * n + 1
-		IsPrime = is_prime(q)
-		if IsPrime:
-			print("4n + 1: {}, Is a prime number: {}, Which prime: {}".format(q, IsPrime, q))
+		qIsPrime = is_prime(q)
+		
+		if qIsPrime:
+			print("4n + 1: {}, Is a prime number: {}, Which prime: {}".format(q, qIsPrime, q))
+			print("{} is the {} Pythagorean prime".format(q, humanize.ordinal(countPythagorean)))
+			countPythagorean += 1
 			sleep(0.3)
 		else:
-				print("4n + 1: {}, Is a prime number: {}, Which composite: {}".format(q, IsPrime, q))
+				print("4n + 1: {}, Is a prime number: {}, Which composite: {}".format(q, qIsPrime, q))
 				sleep(0.3)
+		r = (2 ** n) - 1
+		rIsPrime = is_prime(r)
+		if rIsPrime:
+			print("2 to the n - 1: {}, Is a Mersenne prime {}, Which prime: {}".format(r, rIsPrime, r))
+			print("{} is the {} Mersenne prime".format(r, humanize.ordinal(countMersenne)))
+			countMersenne += 1
+			sleep(0.3)
+		else:
+			print("2 to the n - 1: {}, Is a Mersenne Prime: {}, Which composite: {}".format(r, rIsPrime, r))
+			sleep(0.3)
  			
 Charles()
