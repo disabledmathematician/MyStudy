2394e04811fd6a1c4b8ec2333b75717f79660326
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-01-31 17:46:41 +1100

    Add files via upload

---
+++ ctruscottwatters_charlierscheck.py
@@ -0,0 +1,24 @@
+def CharliersCheck_ctruscottwatters(X, f):
+	topFrequency = max(f)
+	subscriptMax = 0
+	for n in range(len(f)):
+		if f[n] == topFrequency:
+			subscriptMax = n
+	decreaseVal = -subscriptMax - 1
+	increaseVal = len(X) - subscriptMax
+	descendingArray = [n for n in range(0, decreaseVal, -1)]
+	increasingArray = [n for n in range(1, increaseVal)]
+	uArray = descendingArray[::-1] + increasingArray
+	print("top frequency: {}\n subscript max: {}\n f[subscript max]: {}\n decrease val: {}\n increase val: {}\n uArray: {}\n".format(topFrequency, subscriptMax, f[subscriptMax], decreaseVal, increaseVal, uArray))
+	fu = []
+	for n in range(len(uArray)):
+		fu.append(f[n] * uArray[n])
+	print("fu: {}".format(fu))
+	N = sum(f)
+	sumFu = sum(fu)
+	print("Summation of Frequencies: {}\n Summation of the product of f and u: {}\n".format(N, sumFu))
+X = [70, 74, 78, 82, 86, 90, 94, 98, 102, 106, 110, 114, 118, 122, 126]
+f = [4, 9, 16, 28, 45, 66, 85, 72, 54, 38, 27, 18, 11, 5, 2]
+CharliersCheck_ctruscottwatters(X, f)
+			
+			
