5beb4a0c20f66f1835ce27fedb6b11cc5962581b
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-02-01 11:35:09 +1100

    Add files via upload

--- ctruscottwatters_charlierscheck.py
+++ ctruscottwatters_charlierscheck.py
@@ -16,9 +16,10 @@ def CharliersCheck_ctruscottwatters(X, f):
 	print("fu: {}".format(fu))
 	N = sum(f)
 	sumFu = sum(fu)
-	print("Summation of Frequencies: {}\n Summation of the product of f and u: {}\n".format(N, sumFu))
+	print("Summation of Frequencies: {}\nSummation of the product of f and u: {}\n".format(N, sumFu))
 X = [70, 74, 78, 82, 86, 90, 94, 98, 102, 106, 110, 114, 118, 122, 126]
 f = [4, 9, 16, 28, 45, 66, 85, 72, 54, 38, 27, 18, 11, 5, 2]
 CharliersCheck_ctruscottwatters(X, f)
-			
-			
+X = [34.5, 44.5, 54.5, 64.5, 74.5, 84.5, 94.5]
+f = [2, 3, 11, 20, 32, 25, 7]
+CharliersCheck_ctruscottwatters(X, f)
