96c0282c1a45dbd722b53257108e50001eaee46a
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-02-14 22:15:56 +1100

    Add files via upload

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 import math
 from time import sleep
 import humanize
+import sys
+sys.set_int_max_str_digits(1000000)
+""" Charles Thomas Wallace Truscott. Verifying primes in a log2(n) computational complexity.
+
+I love you Dad Mark William Watters, big brother Tai
+
+127 Broken Head Rd Suffolk Park NSW 2481
+
+A use case of Bisection Search to indentify primes. a log2(n) greedy algorithm
+
+"""
+
 def is_prime(n):
     if n <= 1:
         return False
@@ -8,37 +20,35 @@ def is_prime(n):
         return True
     if n % 2 == 0:
         return False
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        if n % i == 0:
-            return False
-
+    low = 0
+    high = n
+    guess = (high + low) / 2.0
+    while n % guess != 0:
+    	if guess ** 2 > n:
+    		high = guess
+    		if n % guess == 0:
+    			return False
+    	elif guess ** 2 < n:
+    	 	low = guess
+    	 	if n % guess == 0:
+    	 		return False
+    	guess = (high + low) / 2.0
     return True
  
 def Charles():
-	countPythagorean = 1
-	countMersenne = 1
-	for n in range(1, 1024):
-		q = 4 * n + 1
-		qIsPrime = is_prime(q)
-		
-		if qIsPrime:
-			print("4n + 1: {}, Is a prime number: {}, Which prime: {}".format(q, qIsPrime, q))
-			print("{} is the {} Pythagorean prime".format(q, humanize.ordinal(countPythagorean)))
-			countPythagorean += 1
-			sleep(0.3)
+	txt = open("FermatNums.txt", "w+")
+	countFermat = 1
+	for n in range(0, 20):
+		s = (2 ** 2 ** n) + 1
+		sIsPrime = is_prime(s)
+		if sIsPrime:
+			print("2 to the 2 to the n + 1: {}, Is a Fermat prime {}, Which prime: {}\n".format(s, sIsPrime, s))
+			txt.write("2 to the 2 to the n + 1: {}, Is a Fermat prime {}, Which prime: {}\n".format(s, sIsPrime, s))
+			print("{} is the {} Fermat prime".format(s, humanize.ordinal(countFermat)))
+			txt.write("{} is the {} Fermat prime\n".format(s, humanize.ordinal(countFermat)))
+			countFermat += 1
 		else:
-				print("4n + 1: {}, Is a prime number: {}, Which composite: {}".format(q, qIsPrime, q))
-				sleep(0.3)
-		r = (2 ** n) - 1
-		rIsPrime = is_prime(r)
-		if rIsPrime:
-			print("2 to the n - 1: {}, Is a Mersenne prime {}, Which prime: {}".format(r, rIsPrime, r))
-			print("{} is the {} Mersenne prime".format(r, humanize.ordinal(countMersenne)))
-			countMersenne += 1
-			sleep(0.3)
-		else:
-			print("2 to the n - 1: {}, Is a Mersenne Prime: {}, Which composite: {}".format(r, rIsPrime, r))
-			sleep(0.3)
- 			
+			print("2 to the 2 to the n + 1: {}, Is a Fermat Prime: {}, Which composite: {}\n".format(s, sIsPrime, s))
+			txt.write("2 to the 2 to the n + 1: {}, Is a Fermat Prime: {}, Which composite: {}\n".format(s, sIsPrime, s))
+	txt.close()
 Charles()
