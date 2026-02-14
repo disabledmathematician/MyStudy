27752f21c71913d79bbefa00a97ba26ded71c539
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-02-14 22:54:05 +1100

    Add files via upload

--- primes.py
+++ primes.py
@@ -25,6 +25,7 @@ def is_prime(n):
     high = len(factors) - 1
     while low <= high:
     	mid = (high + low) // 2
+    	print("Factor tried: {} Index: {}".format(factors[mid], mid))
     	if n % factors[mid] == 0:
     		return False
     	if factors[mid] < n:
@@ -51,3 +52,52 @@ def Charles():
 			txt.write("2 to the 2 to the n + 1: {}, Is a Fermat Prime: {}, Which composite: {}\n".format(s, sIsPrime, s))
 	txt.close()
 Charles()
+
+"""
+Factor tried: 2 Index: 0
+2 to the 2 to the n + 1: 3, Is a Fermat prime True, Which prime: 3
+
+3 is the 1st Fermat prime
+Factor tried: 3 Index: 1
+Factor tried: 4 Index: 2
+2 to the 2 to the n + 1: 5, Is a Fermat prime True, Which prime: 5
+
+5 is the 2nd Fermat prime
+Factor tried: 9 Index: 7
+Factor tried: 13 Index: 11
+Factor tried: 15 Index: 13
+Factor tried: 16 Index: 14
+2 to the 2 to the n + 1: 17, Is a Fermat prime True, Which prime: 17
+
+17 is the 3rd Fermat prime
+Factor tried: 129 Index: 127
+Factor tried: 193 Index: 191
+Factor tried: 225 Index: 223
+Factor tried: 241 Index: 239
+Factor tried: 249 Index: 247
+Factor tried: 253 Index: 251
+Factor tried: 255 Index: 253
+Factor tried: 256 Index: 254
+2 to the 2 to the n + 1: 257, Is a Fermat prime True, Which prime: 257
+
+257 is the 4th Fermat prime
+Factor tried: 32769 Index: 32767
+Factor tried: 49153 Index: 49151
+Factor tried: 57345 Index: 57343
+Factor tried: 61441 Index: 61439
+Factor tried: 63489 Index: 63487
+Factor tried: 64513 Index: 64511
+Factor tried: 65025 Index: 65023
+Factor tried: 65281 Index: 65279
+Factor tried: 65409 Index: 65407
+Factor tried: 65473 Index: 65471
+Factor tried: 65505 Index: 65503
+Factor tried: 65521 Index: 65519
+Factor tried: 65529 Index: 65527
+Factor tried: 65533 Index: 65531
+Factor tried: 65535 Index: 65533
+Factor tried: 65536 Index: 65534
+2 to the 2 to the n + 1: 65537, Is a Fermat prime True, Which prime: 65537
+
+65537 is the 5th Fermat prime
+"""
